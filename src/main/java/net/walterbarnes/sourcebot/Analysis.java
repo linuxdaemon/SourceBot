82e1b9c182c35fd8ffaf9389e661efea8207ba9c
linuxdemon1 <walter@walterbarnes.net>
2016-03-06 10:21:30 -0600

    Extended tag searches to similar tags

--- src/main/java/net/walterbarnes/sourcebot/Analysis.java
+++ src/main/java/net/walterbarnes/sourcebot/Analysis.java
@@ -16,9 +16,14 @@ public class Analysis
 
 		Map<Long, Post> posts = new HashMap<>();
 		Map<Long, List<String>> postData = new HashMap<>();
+		ArrayList<String> tags = new ArrayList<>(Arrays.asList(Config.getTags()));
 
-		for (String tag : Config.getTags())
+		for (int i = 0; i < tags.size(); i++)
 		{
+			long startTime = System.currentTimeMillis();
+			String tag = tags.get(i);
+			System.out.print(tag + " ");
+			int x = 0;
 			loop:
 			for (Post post : tumblr.getPostsFromTag(tag, null, 10000, null))
 			{
@@ -38,22 +43,41 @@ public class Analysis
 				List<String> tagList = postData.get(post.getId());
 				tagList.add(tag);
 				postData.put(post.getId(), tagList);
+
+				for (String t : post.getTags())
+				{
+					if (isTagSimilar(tag, t) && !tags.contains(t))
+					{
+						tags.add(t);
+					}
+				}
+				x++;
 			}
+			System.out.println(x + " posts " + ((System.currentTimeMillis() - startTime) / 1000) + " s");
 		}
 
-		Map<String, Integer> tags = new HashMap<>();
+		Map<String, Integer> tagMap = new HashMap<>();
 
 		for (long postID : postData.keySet())
 		{
-			for (String tag : posts.get(postID).getTags())
+			for (String t : posts.get(postID).getTags())
 			{
-				if (!tags.containsKey(tag))
+				if (!tagMap.containsKey(t))
 				{
-					tags.put(tag, 0);
+					tagMap.put(t, 0);
 				}
-				tags.put(tag, tags.get(tag) + 1);
+				tagMap.put(t, tagMap.get(t) + 1);
 			}
 		}
-		System.out.println(MapHelper.sortByValues((HashMap<String, Integer>) tags));
+		System.out.println(MapHelper.sortByValues((HashMap<String, Integer>) tagMap));
+	}
+
+	private static boolean isTagSimilar(String tag1, String tag2)
+	{
+		if (tag1.contains(tag2) && tag1.length() <= tag2.length() + 5)
+		{
+			return true;
+		}
+		return tag2.contains(tag1) && tag2.length() <= tag1.length() + 5;
 	}
 }
